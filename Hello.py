2c742fef4c0cd70f852564d364f6a237e8c71da2
KINESIZ <tabrisz1741@gmail.com>
2024-07-04 12:15:07 +0700

    เครื่องคิดเลข

--- Hello.py
+++ Hello.py
@@ -1,16 +1,62 @@
-name = "Peerappong"
-Lname = "Borpet"
-x1 = 89 % 5
-print (x1)
-x1 += 5
-x3 = x2 = x1 
-x2 /= 2
+# name = "Peerappong"
+# Lname = "Borpet"
+# x1 = 89 % 5
+# print (x1)
+# x1 += 5
+# x3 = x2 = x1 
+# x2 /= 2
+# 
+# print (x1)
+# print (x2)
+# print (x3)
+# 
+# x1 = x2 = 8
+# x1 &= 9
+# 
+# print (x1)
+# 
+# money = int(input())    
+# if money == 20:
+    # print ("แปปซี่")
+# elif money == 30:
+    # print("โค้ก")
+# elif money == 40:
+    # print("เลย์")
 
-print (x1)
-print (x2)
-print (x3)
 
-x1 = x2 = 8
-x1 &= 9
+# number = int(input("input number "))
+# print("Even Number From 1 to %d",number)
 
-print (x1)
+# i =1
+# while i <= number:
+    # if i % 2 == 1:
+        # print(i)
+    # i += 1
+
+# Name = ["Fiat","N๊ote","Nick","Mossamu","Poน"]
+# for x in Name:
+    # print(Name[1])
+
+def plus(plus,plus2):
+    print("บวก")
+    print(plus," + ",plus2," = ",plus + plus2)
+    
+
+def minus(minus,minus2):
+    print("ลบ")
+    print(minus," - ",minus2," = ",minus - minus2)
+
+def multiply(mul,mul2):
+    print("คูณ")
+    print(mul," x ",mul2," = ",mul * mul2)
+
+def divide(di,di2):
+    print("หาร")
+    print(di," - ",di2," = ",di / di2)
+
+plus(plus=int(input("ใส่ค่าที่ 1 :")),plus2=int(input("ใส่ค่าที่ 2 : ")))
+minus(minus=int(input("ใส่ค่าที่ 1 : ")),minus2=int(input("ใส่ค่าที่ 2 : ")))
+multiply(mul=int(input("ใส่ค่าที่ 1 : ")),mul2=int(input("ใส่ค่าที่ 2 : ")))
+divide(di=int(input("ใส่ค่าที่ 1 : ")),di2=int(input("ใส่ค่าที่ 2 : ")))
+
+
